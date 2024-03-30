333301dfa25f79e954eeb67d6eff4dd3c66ba58b
Nikhil Singh <nikhilsinghmus@gmail.com>
2024-03-29 23:38:29 -0400

    Update clips.

---
+++ _next/static/tvmKtUDqVeUDp-VuMWr9z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-5c5a62dac9220f8f.js","static/chunks/pages/index-00a6a5d9ebc30e4d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
